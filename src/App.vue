469a4eb07a6e51ad5dd6ce4e7cc2ab4b89fc41f6
= <=>
2020-01-10 12:11:33 -0300

    Criando o visual do site

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,73 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div id="slogan" class="text-center">
+      <h1>NameGator</h1>
+      <br>
+      <h6 class="text-secondary">Gerador de nomes usando Vue.js, GraphQL e Node</h6>
+
+      <div id="main">
+        <div class="container">
+          <div class="row">
+            <div class="col-md">
+              <h5>Prefixos <span class="badge badge-info" >{{ prefixes.length }}</span></h5>
+              <div class="card">
+                <div class="card-body">
+                  <ul class="list-group">
+                    <li class="list-group-item" v-for="prefix in prefixes" v-bind:key="prefix">
+                      {{ prefix }}
+                    </li>
+                  </ul>
+                  <br>
+                  <input type="text" class="form-control" placeholder="Digite o prefixo" >
+                </div>
+              </div>
+            </div>
+            <div class="col-md">
+              <h5>Sufixos <span class="badge badge-info" >{{ sufixes.length }}</span></h5>
+              <div class="card">
+                <div class="card-body">
+                    <ul class="list-group">
+                        <li class="list-group-item" v-for="sufix in sufixes" v-bind:key="sufix">
+                          {{ sufix }}
+                        </li>
+                        
+                      </ul>
+                      <br>
+                      <input type="text" class="form-control" placeholder="Digite o sufixo" >
+                </div>
+              </div>
+            </div>
+          </div>
+          <br>
+          <h5>Domínios <span class="badge badge-info" >{{ domains.length }}</span> </h5>
+          <div class="card">
+            <div class="card-body">
+              <ul class="list-group">
+                <li class="list-group-item" v-for="domain in domains" v-bind:key="domain">
+                  {{ domain }}
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import "bootstrap/dist/css/bootstrap.css";
+import "font-awesome/css/font-awesome.css"
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data: function(){
+    return{
+      prefixes: ["Air", "Jet", "Flight"],
+      sufixes: ["Hub", "Station", "Mart"],
+      domains: ["AirHub", "AirStation", "AirMart", "JetHub", "JetStation", "JetMart", "FlightHub", "FlightStation", "FlightMart"]
+    };
   }
 }
 </script>
@@ -24,5 +80,16 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+#slogan{
+  margin-top: 30px;
+  margin-bottom: 30px;
+}
+
+#main{
+  background-color: #F1F1F1;
+  padding-top: 30px;
+  padding-bottom: 30px;
 }
 </style>
